ac2a033db8f5e921cffe1a808f0e6e99ebd7e44b
Rushidikkar3 <168056519+Rushidikkar3@users.noreply.github.com>
2024-12-24 22:09:49 +0530

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+   cin>>n;
+   if(n>=0){
+       cout<<"the number is positive";
+   }
+   else{
+       cout<<"the number is negative";
+   }
+
+    return 0;
+}
